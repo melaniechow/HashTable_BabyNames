db998eda9ef2e8f807affd51e577ffaa8627f171
melaniechow <melaniechow@47e64130-219e-433e-a9cb-5ea3e9b9e2fe>
2018-02-08 08:33:16 +0000

    bucket.c done

--- bucket.c
+++ bucket.c
@@ -5,31 +5,67 @@
 
 bucket* bucket_cons(const char* s, unsigned long int hash, bucket* prev_head)
 {
-  fprintf(stderr,"todo: bucket_cons\n");
-  exit(1);
+	bucket *add_bucket = malloc(sizeof (bucket));
+	add_bucket->string = strdup(s);
+	add_bucket->hash = hash;
+	add_bucket->next = prev_head;
+	return add_bucket;
+  	fprintf(stderr,"todo: bucket_cons\n");
+  	exit(1);
 }
 
 size_t bucket_size(bucket* b)
 {
-  fprintf(stderr,"todo: bucket_size\n");
-  exit(1);
+	size_t num=0;
+	while (b != NULL){
+		num++;
+		b=b->next;
+	}
+	return num;
+ 	fprintf(stderr,"todo: bucket_size\n");
+	exit(1);
 }
 
 int bucket_contains(const char* s, unsigned long int hash, bucket* b)
 {
-  fprintf(stderr,"todo: bucket_contains\n");
-  exit(1);
+	while (b != NULL){
+		if (b->hash == hash){
+			if (strcmp (s, b->string) == 0){
+				return 1;
+			}
+			else {
+				return 0;
+			}
+		}
+		b=b->next;
+	}
+	return 0;
+	fprintf(stderr,"todo: bucket_contains\n");
+	exit(1);
 }
 
 void bucket_show(bucket* b)
 {
-  fprintf(stderr,"todo: bucket_show\n");
-  exit(1);
+	while (b != NULL){
+		fprintf(stdout, "(%s, %lu)", b->string, b->hash); 
+		b=b->next;
+	}
+	fprintf(stderr,"todo: bucket_show\n");
+	exit(1);
 }
 
 void bucket_free(bucket* b)
 {
-  fprintf(stderr,"todo: bucket_free\n");
-  exit(1);
+	while (b != NULL){
+		bucket *to_free=b;
+		b=b->next;
+
+		char *str = malloc(sizeof (char));
+		str=strdup(to_free->string);
+		free(str);
+		free(to_free);
+	}
+	fprintf(stderr,"todo: bucket_free\n");
+	exit(1);
 }
 
--- hset.c
+++ hset.c
@@ -5,31 +5,48 @@
 
 unsigned long int good_hash(const char* s)
 {
-  fprintf(stderr,"todo: good_hash\n");
-  exit(1);
+	unsigned long int ret=17;
+	for (int i=0; i<strlen(s);i++){
+		ret=ret*37 + atoi(s[i]);
+	}
+	return ret;
+	fprintf(stderr,"todo: good_hash\n");
+	exit(1);
 }
 
 unsigned long int bad_hash(const char* s)
 {
-  fprintf(stderr,"todo: bad_hash\n");
-  exit(1);
+	unsigned long int ret = strlen(s);
+	return ret;
+	fprintf(stderr,"todo: bad_hash\n");
+	exit(1);
 }
 
 hset* hset_new(unsigned long int(*hash_func)(const char*), size_t sz)
 {
-  fprintf(stderr,"todo: hset_new\n");
-  exit(1);
+	hset *hset_ret=malloc(sizeof hset);
+	hset_ret->hash_func = hash_func;
+	bucket **new_buckets[sz];
+	hset_ret->buckets=new_buckets;
+	hset_ret->n_buckets=sz;
+	return hset_ret;
+	fprintf(stderr,"todo: hset_new\n");
+	exit(1);
 }
 
 size_t hset_size(hset* hs)
 {
-  fprintf(stderr,"todo: hset_size\n");
-  exit(1);
+	for (int i=0; i<alen(hs->buckets); i++){
+		
+	}
+	fprintf(stderr,"todo: hset_size\n");
+	exit(1);
 }
 
 double hset_load_factor(hset* hs)
 {
   fprintf(stderr,"todo: hset_load_factor\n");
+  return ( /hset_size(hs))
   exit(1);
 }
 
