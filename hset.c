329814f7dbdfad456397de3234e268547df84c85
melaniechow <melaniechow@47e64130-219e-433e-a9cb-5ea3e9b9e2fe>
2018-02-08 08:48:06 +0000

    up to inserting funcs for hset

--- hset.c
+++ hset.c
@@ -26,7 +26,7 @@ hset* hset_new(unsigned long int(*hash_func)(const char*), size_t sz)
 {
 	hset *hset_ret=malloc(sizeof hset);
 	hset_ret->hash_func = hash_func;
-	bucket **new_buckets[sz];
+	bucket **new_buckets= (bucket **)malloc(sizeof (bucket*)*sz);
 	hset_ret->buckets=new_buckets;
 	hset_ret->n_buckets=sz;
 	return hset_ret;
@@ -36,30 +36,43 @@ hset* hset_new(unsigned long int(*hash_func)(const char*), size_t sz)
 
 size_t hset_size(hset* hs)
 {
-	for (int i=0; i<alen(hs->buckets); i++){
-		
+	size_t ret=0;
+	for (int i=0; i<hs->sz; i++){
+		ret+=bucket_size(hs->buckets[i]);
 	}
+	return ret;
 	fprintf(stderr,"todo: hset_size\n");
 	exit(1);
 }
 
 double hset_load_factor(hset* hs)
 {
-  fprintf(stderr,"todo: hset_load_factor\n");
-  return ( /hset_size(hs))
-  exit(1);
+	
+	fprintf(stderr,"todo: hset_load_factor\n");
+	return (hset_size(hs)/(hs->sz))
+	exit(1);
 }
 
 size_t hset_max_bucket(hset* hs)
 {
-  fprintf(stderr,"todo: hset_max_bucket\n");
-  exit(1);
+	size_t max=0;
+	size_t compareVal;
+	for (int i=0; i<hs->sz; i++){
+		compareVal=bucket_size(hs->buckets[i]);
+		if (max < compareVal){
+			max=compareVal;
+		}	
+	}
+	return max;
+	fprintf(stderr,"todo: hset_max_bucket\n");
+	exit(1);
 }
 
 size_t hset_insert(const char* s, hset* hs)
 {
-  fprintf(stderr,"todo: hset_insert\n");
-  exit(1);
+	
+	fprintf(stderr,"todo: hset_insert\n");
+	exit(1);
 }
 
 int hset_contains(const char* s, hset* hs)
